0162adf5a2be6c7575d60676a1e2258d6d5f4b93
BennyLoshop <84132229+BennyLoshop@users.noreply.github.com>
2025-11-23 14:37:47 +0800

    完善了Vapp的API

--- ColumnOS/VAppHost.js
+++ ColumnOS/VAppHost.js
@@ -69,6 +69,8 @@
                 globalVfs: window.globalVfs,
                 globalUtils: window.globalUtils,
                 chunkStore: window.chunkStore,
+                pushToInbox: window.pushToInbox,
+                tokenStore: window.tokenStore,
                 // ============ 增加 getAppFile ============
                 // 传入相对于 vfsRoot 的路径，如 "/img/icon.png"
                 // 返回对应的 Blob
